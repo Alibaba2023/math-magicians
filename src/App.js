43b46a108b23394393873ed1d6feac85d1feb882
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-07-07 00:21:03 +0430

    Quotes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './Components/calculator';
+import Quote from './Components/Quotes';
 
 function App() {
   return (
     <div className="App">
+      <Quote />
       <Calculator />
     </div>
   );
---
+++ src/Components/Quotes.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './Quotes.css';
+
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: { 'X-Api-Key': 'QuNKurqcU+nHATaOqgkxvQ==eMp3O3NdPxMEhBJG' },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error fetching quote');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setQuote(data[0].quote);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  return (
+    <div className="quote-container">
+      <p className="quote-text">{quote}</p>
+    </div>
+  );
+};
+
+export default Quote;
